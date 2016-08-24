57d46f27a67ac009fb1c08a30eb2d99c97e4696c
Adam Williams <awilliams057@student.butte.edu>
2016-08-24 18:16:46 +0000

    lab1

---
+++ Lab1.cpp
@@ -0,0 +1,29 @@
+//Adam Williams
+//Lab1.1
+//24.Aug.2016
+# include <iostream>
+using namespace std;
+using std::cout;
+int main()
+{
+   
+    std::cout<< "   ____||____";
+    cout<< endl;
+    
+    cout<< "  ///////////\\";
+    cout<< endl;
+    
+    cout<< " ////////////\\\\";
+    cout<< endl;
+       
+    cout<< " |    _    |  |";
+    cout<< endl;
+    
+    cout<< " |[] | | []|[]|";
+    cout<< endl;
+    
+    cout<< " |   | |   |  |";
+    cout<< endl;
+    
+    
+}
